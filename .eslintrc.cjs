d2d3d086076fa228c8f6e99385315de8b1226c88
valery-chumak <valeriia.chumak@binary-studio.com>
2024-12-16 14:28:00 +0200

    fix setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,8 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/essential",
+    "plugin:vue/recommended",
     "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
   ],
 };
